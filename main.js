0cf44ec2a830d083fad71cf762329e4e3f50cf89
dasom im <dim@student.42seoul.kr>
2023-04-30 07:39:07 +0900

    Refactor: Modal 구현,작동 완료

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const getMemofromLocalStorage = () => {
-	const memo = JSON.parse(localStorage.getItem('memoNote'));
-	return memo || [];
-}
 
 const createLi = (className) => {
 	const $li = document.createElement('li');
@@ -9,43 +5,10 @@ const createLi = (className) => {
 	return $li;
 }
 
-const createLabel = (id, className, text) => {
-	const $label = document.createElement('label');
-	$label.setAttribute('for', id);
-	if (className) $label.className = className;
-	if (text) $label.textContent = text;
-	return $label;
-}
-
-const createInput = (id, type, isRequired, isReadOnly) => {
-	const $input = document.createElement('input');
-	$input.setAttribute('id', id);
-	if (type) $input.setAttribute('type', type);
-	if (isRequired) $input.required = true;
-	if (isReadOnly) $input.readOnly = true;
-	return $input;
-}
-
-const createTextarea = (id, rows, isRequired, isReadOnly) => {
-	const $textarea = document.createElement('textarea');
-	$textarea.setAttribute('id', id);
-	if (rows) $textarea.setAttribute('rows', rows);
-	if (isRequired) $textarea.required = true;
-	if (isReadOnly) $textarea.readOnly = true;
-	return $textarea;
-}
-
-const createButton = (className, text) => {
-	const $button = document.createElement('button');
-	$button.setAttribute('class', className);
-	if (text) $button.textContent = text;
-	return $button;
-}
-
-const addButtonEventListener = ($btnAdd, $modal, $inputMemoTitle, $textarea) => {
+const addButtonEventListener = ($btnAdd, $modal, $inputMemoTitle, $inputContent) => {
 	$btnAdd.addEventListener('click', function () {
 		const title = $inputMemoTitle.value;
-		const content = $textarea.value;
+		const content = $inputContent.value;
 		if (!title) {
 			alert('타이틀을 입력해주세요');
 			return ;
@@ -57,9 +20,9 @@ const addButtonEventListener = ($btnAdd, $modal, $inputMemoTitle, $textarea) => 
 			'id': id,
 		};
 		localMemo.push(memo);
-		localStorage.setItem('memoNote', JSON.stringify(localMemo));
+		localStorage.setItem('memo', JSON.stringify(localMemo));
 		$inputMemoTitle.value = '';
-		$textarea.value = '';
+		$inputContent.value = '';
 		$modal.close();
 		render();
 	})
@@ -77,43 +40,43 @@ const addNewMemoEventListener = () => {
 		const $modal = document.querySelector('.modal');
 		$modal.innerHTML = `
 			<button class="btn-close-memo">x</button>
-			<label for="memo-title" class="a11y-hidden"></label>
-			<input type="text" id="memo-title" />
-			<label for="memo-content" class="a11y-hidden"></label>
+			<label for="new-memo-title" class="a11y-hidden"></label>
+			<input type="text" id="new-memo-title" />
+			<label for="new-memo-content" class="a11y-hidden"></label>
 			<textarea
-				name="memo-content"
-				id="memo-content"
+				name="new-memo-content"
+				id="new-memo-content"
 				cols="30"
 				rows="10"
 			></textarea>
 			<button class="btn-add-memo">Add</button>
 		`
-		const $inputTitle = document.querySelector('.modal #memo-title');
-		const $inputContent = document.querySelector('.modal #memo-content');
-		const $btnClose = document.querySelector('.btn-close-memo');
-		const $btnAdd = document.querySelector('.btn-add-memo');
+		const $inputTitle = $modal.querySelector('#new-memo-title');
+		const $inputContent = $modal.querySelector('#new-memo-content');
+		const $btnClose = $modal.querySelector('.btn-close-memo');
+		const $btnAdd = $modal.querySelector('.btn-add-memo');
 		addButtonEventListener($btnAdd, $modal, $inputTitle, $inputContent);
 		closeButtonEventListener($btnClose, $modal);
 		$modal.showModal();
 	});
 }
 
-const editButtonEventListener = ($btnEdit	,$inputMemoTitle, $textarea) => {
+const editButtonEventListener = ($btnEdit, $inputTitle, $inputContent) => {
 	$btnEdit.addEventListener('click', function() {
-		$inputMemoTitle.readOnly = false;
-		$textarea.readOnly = false;
+		console.log('#@#@#@');
+		$inputTitle.readOnly = false;
+		$inputContent.readOnly = false;
 		$btnEdit.textContent = 'Save';
 		$btnEdit.addEventListener('click', function() {
-			const id = $inputMemoTitle.getAttribute('name');
-			console.log(id);
+			const id = $inputTitle.getAttribute('data-id');
 			for(let memo of localMemo) {
 				if (memo.id == id) {
-					memo.title = $inputMemoTitle.value;
-					memo.content = $textarea.value;
+					memo.title = $inputTitle.value;
+					memo.content = $inputContent.value;
 					break;
 				}
 			}
-			localStorage.setItem('memoNote', JSON.stringify(localMemo));
+			localStorage.setItem('memo', JSON.stringify(localMemo));
 			render();
 		})
 	})
@@ -121,15 +84,10 @@ const editButtonEventListener = ($btnEdit	,$inputMemoTitle, $textarea) => {
 
 const deleteButtonEventListener = ($btnDelete, $inputMemoTitle) => {
 	$btnDelete.addEventListener('click', function() {
-		const id = $inputMemoTitle.getAttribute('name');
-		console.log(id);
+		const id = $inputMemoTitle.getAttribute('data-id');
 		const index = localMemo.findIndex((memo) => memo.id == id);
-		console.log('index: ',index);
-		console.log(localMemo);
 		localMemo.splice(index, 1);
-		console.log(localMemo);
-
-		localStorage.setItem('memoNote', JSON.stringify(localMemo));
+		localStorage.setItem('memo', JSON.stringify(localMemo));
 		render();
 	})
 }
@@ -137,26 +95,36 @@ const deleteButtonEventListener = ($btnDelete, $inputMemoTitle) => {
 const displayMemo = () => {
 	const $container = document.querySelector('.container');
 	for(let i = localMemo.length - 1; i >= 0; i--) {
-		const $liMemo = createLi('li-existing-memo');
-		const $labelMemoTitle = createLabel('memo-title', 'a11y-hidden', 'Memo Title');
-		const $inputMemoTitle = createInput('memo-title', 'text', false, true);
-		$inputMemoTitle.value = localMemo[i].title;
-		$inputMemoTitle.setAttribute('name', localMemo[i].id);
-		const $labelMemoContent = createLabel('memo-content', 'a11y-hidden', 'Memo Content');
-		const $textarea = createTextarea('memo-content', '8', false, true);
-		$textarea.value = localMemo[i].content;
-		const $btnEdit = createButton('btn-edit-memo', 'Edit');
-		const $btnDelete = createButton('btn-delete-memo', 'Delete');
-		$liMemo.appendChild($labelMemoTitle);
-		$liMemo.appendChild($inputMemoTitle);
-		$liMemo.appendChild($labelMemoContent);
-		$liMemo.appendChild($textarea);
-		$liMemo.appendChild($btnEdit);
-		$liMemo.appendChild($btnDelete);
+		const $liMemo = createLi('.li-existing-memo');
 		$container.appendChild($liMemo);
-		editButtonEventListener($btnEdit, $inputMemoTitle, $textarea);
-		deleteButtonEventListener($btnDelete, $inputMemoTitle);
+		$liMemo.innerHTML = `
+      <label for="memo-title" class="a11y-hidden">Memo Title</label
+      ><input
+				id="memo-title"
+				type="text" 
+				data-id="${localMemo[i].id}"
+				readonly
+      /><label for="memo-content" class="a11y-hidden">Memo Content</label
+      ><textarea id="memo-content" rows="8" readonly></textarea
+      ><button class="btn-edit-memo">Edit</button
+      ><button class="btn-delete-memo">Delete</button>
+		`
+		const $btnEdit = $liMemo.querySelector('.btn-edit-memo');
+		const $btnDelete = $liMemo.querySelector('.btn-delete-memo');
+		const $inputTitle = $liMemo.querySelector('#memo-title');
+		const $inputContent = $liMemo.querySelector('#memo-content');
+		$inputTitle.value = localMemo[i].title;
+		$inputContent.value = localMemo[i].content;
+		console.log($inputTitle.value, $inputContent.value);
+		editButtonEventListener($btnEdit, $inputTitle, $inputContent);
+		deleteButtonEventListener($btnDelete, $inputTitle);
 	}
+}
+
+const render = () => {
+	const $container = document.querySelector('.container');
+	$container.innerHTML = '';
+	if (localMemo.length) displayMemo();
 }
 
 const start = () => {
@@ -164,10 +132,9 @@ const start = () => {
 	render();
 }
 
-const render = () => {
-	const $container = document.querySelector('.container');
-	$container.innerHTML = '';
-	if (localMemo.length) displayMemo();
+const getMemofromLocalStorage = () => {
+	const memo = JSON.parse(localStorage.getItem('memo'));
+	return memo || [];
 }
 
 const localMemo = getMemofromLocalStorage();
